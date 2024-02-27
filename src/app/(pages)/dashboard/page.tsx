13bcd4ac5b353dfe03fdfdd87200c10e3e7d3905
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2024-02-27 18:12:04 -0300

    feat: create folder structure and install dependencies

---
+++ src/app/(pages)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <h1>
+        Painel administrativo exibindo numero de inscritos e artigos submetidos
+      </h1>
+    </main>
+  );
+}
